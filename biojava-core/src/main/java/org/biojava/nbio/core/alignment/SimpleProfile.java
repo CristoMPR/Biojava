9afc4c62ba4f7adcabfd1e9e9ddfb97cf03a9101
CristoMPR <alu0101218007@ull.edu.es>
2022-01-07 11:37:34 +0000

    Tercer Requisito - ToString

--- biojava-core/src/main/java/org/biojava/nbio/core/alignment/SimpleProfile.java
+++ biojava-core/src/main/java/org/biojava/nbio/core/alignment/SimpleProfile.java
@@ -398,7 +398,7 @@ public class SimpleProfile<S extends Sequence<C>, C extends Compound> implements
 
 	@Override
 	public String toString() {
-		return toString(getLength(), null, IOUtils.getIDFormat(list), true, true, true, true, true, false);
+		return toString(getLength(), null, IOUtils.getIDFormat(list), true, false, false, false, false, false);
 	}
 
 	// method for Iterable
